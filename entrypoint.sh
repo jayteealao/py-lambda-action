c3c4d1792d66ed573a5a2fa528d096c7ccc0f28d
jayteealao <jayteealao@gmail.com>
2021-09-01 14:37:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,26 +8,15 @@ install_zip_dependencies(){
 	zip -r dependencies.zip ./python
 }
 
-publish_dependencies_as_layer(){
-	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-	LAYER_VERSION=$(jq '.Version' <<< "$result")
-	rm -rf python
-	rm dependencies.zip
-}
-
-update_function_layers(){
-	echo "Using the layer in the function..."
-	aws lambda update-function-configuration --function-name populate_bev_tables --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
-	aws lambda update-function-configuration --function-name bevtest --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+publish_function_bevtest(){
+	echo "Deploying the code itself..."
+	zip -r bevtest.zip beteyeview
+	aws lambda update-function-code --function-name bevtest --zip-file fileb://bevtest.zip
 }
 
 deploy_lambda_function(){
 	install_zip_dependencies
-	publish_dependencies_as_layer
-# 	publish_function_populate
-# 	publish_function_bevtest
-	update_function_layers
+	publish_function_bevtest
 }
 
 deploy_lambda_function
